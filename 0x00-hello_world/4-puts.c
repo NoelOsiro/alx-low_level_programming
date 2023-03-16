c22d6981f9575a0b79988a0e8c95a1f95f1814c0
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-17 00:56:22 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 /*
  * File: 4-puts.c
- * Prints a string, followed by a new line.
  */
 
 #include <stdio.h>
-int main(void)
+/*
+* Prints a string, followed by a new line.
+* Return 0
+*/
+int main(void)	
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
