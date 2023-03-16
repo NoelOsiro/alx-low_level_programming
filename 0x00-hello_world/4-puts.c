57efd41e07d3107fbad6c80fc12188cf8458584d
NoelOsiro <noel.osiro@azubiafrica.org>
2023-03-16 14:47:58 -0700

    Added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/*
+ * File: 4-puts.c
+ *Prints "Programming is like building a multilingual puzzle, followed by a new line and  Return: 0.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
