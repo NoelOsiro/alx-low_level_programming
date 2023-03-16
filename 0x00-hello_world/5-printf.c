e79c4a9851cff8f15eb6ce2374343ca24467c777
NoelOsiro <noel.osiro@azubiafrica.org>
2023-03-16 15:10:23 -0700

    Added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/*
+ * File: 5-printf.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints string using printf followed by a new line.
+ * Return: 0.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
