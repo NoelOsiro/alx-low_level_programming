01c183b12a8bca15d0d95a0ed4ad19a73ed4ef0a
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-17 17:59:09 +0300

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+/* File: 6-print_numbers.c
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers only Base 10.
+ *
+ * Return: 0 
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
