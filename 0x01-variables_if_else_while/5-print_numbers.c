51a78c490929643a197f52dcdc4c3f1a582dc6da
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-17 17:55:51 +0300

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/* File: 5-print_numbers.c
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: 0 
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
