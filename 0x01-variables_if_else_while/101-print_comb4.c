c509070d9dc5b30bc5487b29d3472491938f017b
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-17 18:53:20 +0300

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+/* File: 101-print_comb4.c */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of three different digits,
+ * Return: 0.
+ */
+int main(void)
+{
+	int no1, no2, no3;
+
+	for (no1 = 0; no1 < 8; no1++)
+	{
+		for (no2 = no1 + 1; no2 < 9; digit2++)
+		{
+			for (no3 = digit2 + 1; digit3 < 10; no3++)
+			{
+				putchar((no1 % 10) + '0');
+				putchar((no2 % 10) + '0');
+				putchar((no3 % 10) + '0');
+
+				if (no1 == 7 && no2 == 8 && no3 == 9)
+					continue;
+				
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
