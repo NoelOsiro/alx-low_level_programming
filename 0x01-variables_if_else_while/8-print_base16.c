0dfa6cd3d3e2b0d808a284abd415d6717e8e81e7
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-17 18:20:01 +0300

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+/* File: 8-print_base16.c */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char letters;
+
+	for (i = 0; i < 10; i++)
+  {
+    putchar((i % 10) + '0');
+  }
+	for (letters = 'a'; letters <= 'f'; letters++)
+  {
+		putchar(letters);
+  }
+	putchar('\n');
+	return (0);
+}
