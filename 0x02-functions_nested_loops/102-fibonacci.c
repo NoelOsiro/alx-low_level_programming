161ba638d09d44fc9cde945b6c068525d048729a
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-21 23:17:17 +0000

    final

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,29 +1,25 @@
-/*
- * File: 102-fibonacci.c
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ * main - Prints the add of the Fibonacci numbers
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-    int i;
+	int c;
+	long int n1, n2, fn;
 
-    
-    int t1 = 0, t2 = 1;
-    int nextTerm = t1 + t2;
-
-    printf("%d, %d, ", t1, t2);
-    for (i = 3; i <= 50; ++i) {
-        printf("%d, ", nextTerm);
-        t1 = t2;
-        t2 = nextTerm;
-        nextTerm = t1 + t2;
-    }
-  return 0;
+	n1 = 1;
+	n2 = 2;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 48; c++)
+	{
+		fn = n1 + n2;
+		printf(", %ld", fn);
+		n1 = n2;
+		n2 = fn;
+	}
+	printf("\n");
+	return (0);
 }
