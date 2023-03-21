5d669ab02820905311270603c59d1343cd801380
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-21 22:13:15 +0000

    Fibonnacci

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+/*
+ * File: 11-print-to98.c.c
+ */
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    int num;
+    int sum = 0;
+
+    for (num = 0; num <= 1024; num++)
+    {
+        if ((num % 3) == 0 || (num % 5) == 0)
+        {
+            sum += num;
+        }
+	printf("%d\n", sum);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/102.fibonacci.c
@@ -0,0 +1,29 @@
+/*
+ * File: 102-fibonacci.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    int i, n;
+
+    
+    int t1 = 0, t2 = 1;
+    int nextTerm = t1 + t2;
+
+    printf("%d, %d, ", t1, t2);
+    for (i = 3; i <= 50; ++i) {
+        printf("%d, ", nextTerm);
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
+    }
+  return 0;
+}
