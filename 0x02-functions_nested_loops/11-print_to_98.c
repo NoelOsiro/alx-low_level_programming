254718a394ec5bb32a4b089555ca71b0ae62cc4f
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-21 22:42:41 +0000

    Renamed fibonacci.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,6 +3,14 @@
  */
 #include <stdio.h>
 #include "main.h"
+/**
+ * print_to_98 - Prints all natural numbers
+ * from n to 98.
+ *
+ * @n: input number.
+ *
+ * Return: no return.
+ */
 void print_to_98(int n)
 {
 	if (n > 98)
