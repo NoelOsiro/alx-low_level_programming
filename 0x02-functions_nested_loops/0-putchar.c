fc3371525c04dc7346bedab4684eb9ee1baa7320
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-21 16:05:50 +0000

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+/*
+ * File: 0-putchar.c
+ */
+
+#include<stdio.h> 
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
