7e79dbde7056a8d869629f216a9ee8f431b524e1
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-21 22:59:07 +0000

    Fixed all

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,43 +5,51 @@
  *
  * Return: Always 0.
  */
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int c, boolean, boolean;
-	long fn, ft2, t11, t22;
+	int c, boolean, boolean2;
+	long int n1, n2, fn, fn2, n11, n22;
 
-	int t1 = 0, t2 = 1;
-	boolean =  boolean = 1;
-	printf("%ld, %ld", t1, t2);
+	n1 = 1;
+	n2 = 2;
+	boolean =  boolean2 = 1;
+	printf("%ld, %ld", n1, n2);
 	for (c = 0; c < 96; c++)
 	{
 		if (boolean)
 		{
-			fn = t1 + t2;
+			fn = n1 + n2;
 			printf(", %ld", fn);
-			t1 = t2;
-			t2 = fn;
+			n1 = n2;
+			n2 = fn;
 		}
 		else
 		{
-			if (boolean)
+			if (boolean2)
 			{
-				t11 = t1 % 1000000000;
-				t22 = t2 % 1000000000;
-				t1 = t1 / 1000000000;
-				t2 = t2 / 1000000000;
-				boolean = 0;
+				n11 = n1 % 1000000000;
+				n22 = n2 % 1000000000;
+				n1 = n1 / 1000000000;
+				n2 = n2 / 1000000000;
+				boolean2 = 0;
 			}
-			ft2 = (t11 + t22);
-			fn = t1 + t2 + (ft2 / 1000000000);
+			fn2 = (n11 + n22);
+			fn = n1 + n2 + (fn2 / 1000000000);
 			printf(", %ld", fn);
-			printf("%ld", ft2 % 1000000000);
-			t1 = t2;
-			t11 = t22;
-			t2 = fn;
-			t22 = (ft2 % 1000000000);
+			printf("%ld", fn2 % 1000000000);
+			n1 = n2;
+			n11 = n22;
+			n2 = fn;
+			n22 = (fn2 % 1000000000);
 		}
-		if (((t1 + t2) < 0) && boolean == 1)
+		if (((n1 + n2) < 0) && boolean == 1)
 			boolean = 0;
 	}
 	printf("\n");
