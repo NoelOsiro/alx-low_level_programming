2440918f0ab8f5acf182c030055c9a087a3b9470
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-21 19:36:08 +0000

    Added 2-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+/*
+ * File: 1-alphabet.c
+ */
+
+#include<stdio.h> 
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	int count,j = 0;
+	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	while (count++ <= 9)
+	{
+		for (j = 0; j <= 26; j++)
+			_putchar(letters[j]);
+		_putchar('\n');
+	}
+}
