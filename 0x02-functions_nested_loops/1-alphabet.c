0e5d0375375edf1335b1c0a6e1f1ede724fcf688
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-21 16:05:29 +0000

    Added 0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+/*
+ * File: 1-alphabet.c
+ */
+
+#include<stdio.h> 
+/**
+ * print_alphabet - Prints the alphabet, in lowercase.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+    char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	printf("%s",letters);
+    printf("\n");
+
+}
