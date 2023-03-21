e591e0f2c47e31081de7e4d58aba88060f4873a9
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-21 21:20:46 +0000

    Trailing whitespace

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,8 +6,9 @@
 /**
  * _abs - Computes the absolute value of an integer.
  * @n: The integer to be checked.
+ * 
  * Return: The absolute value of the integer.
- *         The standard library provides a similar function: abs.     
+ *         The standard library provides a similar function: abs.
  */
 int _abs(int n)
 {
