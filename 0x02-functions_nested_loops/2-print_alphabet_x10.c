e3211ed49a99170b8ac1d2a7acce7d2ad5c397b5
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-21 20:58:27 +0000

    trailing whitespace

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,19 +4,14 @@
 #include<stdio.h>
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
-	int count  = 0;
-	int i;
+	char letter;
 
-	while (count++ <= 9)
-	{    
-    	for (i = 97; i <= 122; i++)
-    	{
-        	printf("%c", i);
-    	}
-		_putchar('\n');
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
