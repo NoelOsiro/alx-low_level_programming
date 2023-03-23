c56ebab513729be237962b84b447ede523d4364e
NoelOsiro <noel.osiro@azubiafrica.org>
2023-03-23 12:19:04 -0700

    Added 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -11,13 +11,7 @@ int main(void)
 {
 	int i;
 
-<<<<<<< HEAD
-  	i = 0;
- 	 positive_or_negative(i);
+	i = 98;
+  	positive_or_negative(i);
   	return (0);
-=======
-  i = 98;
-  positive_or_negative(i);
-  return (0);
->>>>>>> d455795ddd454c37fe10389a1e339029248e2a50
 }
