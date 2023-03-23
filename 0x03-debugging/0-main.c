c2666e99aed044e2fff1e8253f8b90a7b768c51c
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-23 22:49:40 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
-
 /**
- * main - Test function for positive or negative
- * Return: 0
+ * main - Tests positive_or_negative function
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
