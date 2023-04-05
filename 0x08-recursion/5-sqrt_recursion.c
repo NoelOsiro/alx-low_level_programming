81b6b266893965b0bdae953dcb15e8eef4279b4b
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-04-05 23:09:09 +0000

    added palindrome

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,15 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _sqrt_recursion - Returns the natural square root of a number.
+ * _sqrt_helper - Performs a binary search to find the square root in the range [1, n/2].
  * @n: The number to return the square root of.
+ * @low: low (the lower bound of the search range).
+ * @high: High (the upper bound of the search range)
  *
  * Return: If n has a natural square root - the natural square root of n.
  *         If n does not have a natural square root - -1.
  */
-#include <stdio.h>
-
 int _sqrt_helper(int n, int low, int high) {
     if (low > high)
     {
@@ -27,6 +29,13 @@ int _sqrt_helper(int n, int low, int high) {
     }
 }
 
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
+ *
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
+ */
 int _sqrt_recursion(int n) {
     if (n < 0)
     {
---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
+int is_palindrome(char *s)
+{
+    int len = strlen(s);
+    for (int i = 0; i < len / 2; i++)
+    {
+        if (s[i] != s[len - i - 1])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
