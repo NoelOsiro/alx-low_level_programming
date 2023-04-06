37b6628556da55cb3fb5dca9b1885591d6a38242
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-04-06 00:47:42 +0000

    betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,47 +1,27 @@
 #include "main.h"
-#include <stdio.h>
-int _sqrt_helper(int n, int low, int high);
+
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+
 /**
- * _sqrt_helper - Performs a binary search to find the square root.
- * @n: The number to return the square root of.
- * @low: low (the lower bound of the search range).
- * @high: High (the upper bound of the search range)
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
  *
- * Return: If n has a natural square root - the natural square root of n.
- *         If n does not have a natural square root - -1.
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
  */
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
 
+	if (root == num / 2)
+		return (-1);
 
-/* Returns the natural square root of n if it exists, otherwise -1 */
-/* Helper function that performs a binary search for the square root */
-int _sqrt_helper(int n, int low, int high)
-{
-if (low > high)
-{
-/* The square root does not exist */
-return (1);
+	return (find_sqrt(num, root + 1));
 }
-int mid;
 
-mid = (low + high) / 2;
-int square = (mid *mid);
-
-if (square == n)
-{
-/* Found the square root */
-return (mid);
-}
-else if (square < n)
-{
-/* The square root is greater than mid */
-return (_sqrt_helper(n, mid + 1, high));
-}
-else
-{
-/* The square root is less than mid */
-return (_sqrt_helper(n, low, mid - 1));
-}
-}
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
  * @n: The number to return the square root of.
@@ -49,12 +29,15 @@ return (_sqrt_helper(n, low, mid - 1));
  * Return: If n has a natural square root - the natural square root of n.
  *         If n does not have a natural square root - -1.
  */
-
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-{
-return (-1);
-}
-return (_sqrt_helper(n, 0, n));
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
 }
