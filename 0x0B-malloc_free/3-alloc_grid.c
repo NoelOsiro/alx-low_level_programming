43be8e44de586933bb8e0d9462c261ec64364650
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-04-12 17:07:27 +0000

    Added 3-alloc_grind.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,36 +12,36 @@
 
 int **alloc_grid(int width, int height)
 {
-
 int **grid;
 int i, j;
 
 if (width <= 0 || height <= 0)
-{
 return (NULL);
-}
 
-grid = malloc(height * sizeof(int));
+grid = malloc(sizeof(int *) * height);
+
 if (grid == NULL)
-{
 return (NULL);
-}
+
 for (i = 0; i < height; i++)
 {
-grid[i] = malloc(width * sizeof(int));
+grid[i] = malloc(sizeof(int) * width);
+
 if (grid[i] == NULL)
 {
-for (j = 0; j < i; j++)
-{
-free(grid[j]);
-}
+for (; i >= 0; i--)
+free(grid[i]);
+
 free(grid);
 return (NULL);
 }
-for (j = 0; j < width; j++)
+}
+
+for (i = 0; i < height; i++)
 {
+for (j = 0; j < width; j++)
 grid[i][j] = 0;
 }
-}
+
 return (grid);
 }
