8a7d4be5ebbeacb3b91f0dd53342f66d4fa33010
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-04-20 23:26:19 +0200

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/*
- * File: function_pointers.h
- * AUth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0E-function_pointers directory.
- */
 
 #include <stdlib.h>
 
