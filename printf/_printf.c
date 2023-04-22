6b82307366d521acc2af6f601d8ba836b096916b
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-04-22 14:14:06 +0200

    Update _printf.c

--- printf/_printf.c
+++ printf/_printf.c
@@ -1,126 +1,85 @@
-#include "main.h"
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdlib.h>
+#include <stdarg.h> // for va_list, va_start, and va_end
+#include <unistd.h> // for write
+#include <stddef.h> // for size_t
 
-// Writes a single character to stdout
-static void write_char(char c, int *num_chars_printed) {
-    write(1, &c, 1);
-    (*num_chars_printed)++;
-}
-
-// Writes a string to stdout
-static void write_string(char *s, int *num_chars_printed) {
-    int len = 0;
-    while (s[len] != '\0') {
-        len++;
-    }
-    write(1, s, len);
-    (*num_chars_printed) += len;
-}
-
-// Writes an integer to stdout
-static void write_integer(int num, int *num_chars_printed) {
-    char buf[32];
-    int len = snprintf(buf, sizeof(buf), "%d", num);
-    write(1, buf, len);
-    (*num_chars_printed) += len;
-}
-
-// Writes an unsigned integer to stdout in the specified base
-static void write_unsigned_int(unsigned int num, int base, bool is_uppercase_hex, int *num_chars_printed) {
-    char buf[32];
-    if (is_uppercase_hex) {
-        // Convert to uppercase hex letters
-        for (int i = 0; i < sizeof(buf); i++) {
-            buf[i] = '\0';
-        }
-        int i = sizeof(buf) - 1;
-        do {
-            int digit = num % base;
-            buf[i--] = (digit < 10) ? ('0' + digit) : ('A' + digit - 10);
-            num /= base;
-        } while (num != 0);
-        i++; // Adjust i to first non-zero index
-        for (; i < sizeof(buf); i++) {
-            if (buf[i] >= 'a' && buf[i] <= 'z') {
-                buf[i] = buf[i] - 'a' + 'A';
-            }
-        }
-    } else {
-        // Lowercase hex letters or octal or decimal
-        int len = snprintf(buf, sizeof(buf), (base == 8) ? "%o" : (base == 16) ? "%x" : "%u", num);
-    }
-    int len = 0;
-    while (buf[len] != '\0') {
-        len++;
-    }
-    write(1, buf, len);
-    (*num_chars_printed) += len;
-}
-
-// Main printf function
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
     va_list args;
     va_start(args, format);
 
-    int num_chars_printed = 0;
+    char buffer[1024];
+    int i = 0, j = 0;
+    int buf_idx = 0;
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            // Handle conversion specifier
-            format++;
-            bool is_unsigned = false;
-            switch (*format) {
-                case 'c': {
-                    char c = va_arg(args, int);
-                    write_char(c, &num_chars_printed);
+    // iterate through the format string
+    while (format[j])
+    {
+        // handle conversion specifier
+        if (format[j] == '%')
+        {
+            j++;
+
+            // handle unknown conversion specifier
+            if (!format[j])
+            {
+                buffer[i++] = '%';
+                break;
+            }
+
+            // handle recognized conversion specifier
+            switch (format[j])
+            {
+                case 'b':
+                    // handle binary conversion
                     break;
-                }
-                case 's': {
-                    char *s = va_arg(args, char*);
-                    write_string(s, &num_chars_printed);
-                    break;
-                }
                 case 'd':
-                case 'i': {
-                    int num = va_arg(args, int);
-                    write_integer(num, &num_chars_printed);
+                case 'i':
+                    // handle decimal and integer conversion
                     break;
-                }
-                case 'u': {
-                    unsigned int num = va_arg(args, unsigned int);
-                    write_unsigned_int(num, 10, false, &num_chars_printed);
+                case 'c':
+                    // handle character conversion
                     break;
-                }
-                case 'o': {
-                            unsigned int num = va_arg(args, unsigned int);
-                            write_unsigned_int(num, 8, false, &num_chars_printed);
-                            break;
-                            }
-                case 'X': {
-                            is_unsigned = true;
-                                // fall through to handle as uppercase hexadecimal
-                            }
-                case 'x': {
-                            unsigned int num = is_unsigned ? va_arg(args, unsigned int) : va_arg(args, int);
-                            bool is_uppercase_hex = (*format == 'X');
-                            write_unsigned_int(num, 16, is_uppercase_hex, &num_chars_printed);
-                            break;
-                            }
-            }                   
+                case 's':
+                    // handle string conversion
+                    break;
+                case 'x':
+                    // handle lowercase hexadecimal conversion
+                    break;
+                case 'X':
+                    // handle uppercase hexadecimal conversion
+                    break;
+                default:
+                    // unknown conversion specifier, print character as is
+                    buffer[i++] = '%';
+                    buffer[i++] = format[j];
+                    break;
+            }
         }
-        else {
-            // Handle regular character
-            write(1, format, 1); // write character to stdout
-            num_chars_printed++;
+        // handle regular character
+        else
+        {
+            buffer[i++] = format[j];
         }
 
-        format++;
+        // flush buffer if necessary
+        if (i >= 1023)
+        {
+            buffer[i] = '\0';
+            write(1, buffer, i);
+            i = 0;
+        }
+
+        j++;
+    }
+
+    // flush remaining buffer
+    if (i > 0)
+    {
+        buffer[i] = '\0';
+        write(1, buffer, i);
     }
 
     va_end(args);
 
-    return num_chars_printed;
+    return i;
 }
