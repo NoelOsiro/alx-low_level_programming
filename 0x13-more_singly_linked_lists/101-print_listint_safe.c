217258829a537c7d66d8811a439c30e686a937c6
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-05-02 22:30:13 +0000

    102

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,51 +1,39 @@
+#include <stdlib.h>
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint_safe - Prints a listint_t list safely.
- * @head: A pointer to the head of the listint_t list.
+ * print_listint_safe - prints a listint_t linked list safely
+ * @head: pointer to the head node of the list
  *
- * Return: The number of nodes in the list.
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *tortoise = head, *hare = head;
 	size_t count = 0;
 
-	while (hare != NULL && hare->next != NULL)
+	while (tortoise != NULL && hare != NULL && hare->next != NULL)
 	{
+		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
 		tortoise = tortoise->next;
 		hare = hare->next->next;
 
-		if (hare == tortoise)
+		if (tortoise == hare)
 		{
-			printf("-> [%p] %d\n", (void *)hare, hare->n);
+			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
 			count++;
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+				count++;
+				tortoise = tortoise->next;
+			}
 			break;
 		}
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+
 		count++;
 	}
-	if (hare == NULL || hare->next == NULL)
-	{
-		while (tortoise != NULL)
-		{
-			printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-			count++;
-			tortoise = tortoise->next;
-		}
-	}
-	else
-	{
-		tortoise = tortoise->next;
-		while (tortoise != hare)
-		{
-			printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-			count++;
-			tortoise = tortoise->next;
-		}
-		printf("-> [%p] %d\n", (void *)hare, hare->n);
-		count++;
-	}
+
 	return (count);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,3 +1,45 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - Frees a listint_t list safely (ie.
+ *					 can free lists containing loops)
+ * @h: A pointer to the address of
+ *	 the head of the listint_t list.
+ *
+ * Return: The size of the list that was freed.
+ *
+ * Description: The function sets the head to NULL.
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current, *temp;
+	size_t count = 0;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current = *h;
+	while (current != NULL)
+	{
+		count++;
+		if (current < current->next)
+		{
+			temp = current;
+			current = current->next;
+			free(temp);
+		}
+		else
+		{
+			free(current);
+			*h = NULL;
+			break;
+		}
+	}
+
+	return (count);
+}
+
 
 #include <stdlib.h>
 #include "lists.h"
@@ -10,32 +52,32 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tortoise = head, *hare = head;
-	size_t count = 0;
+    const listint_t *tortoise = head, *hare = head;
+    size_t count = 0;
 
-	while (tortoise != NULL && hare != NULL && hare->next != NULL)
-	{
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		tortoise = tortoise->next;
-		hare = hare->next->next;
+    while (tortoise != NULL && hare != NULL && hare->next != NULL)
+    {
+        printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+        tortoise = tortoise->next;
+        hare = hare->next->next;
 
-		if (tortoise == hare)
-		{
-			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
-			count++;
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-				count++;
-				tortoise = tortoise->next;
-			}
-			break;
-		}
+        if (tortoise == hare)
+        {
+            printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+            count++;
+            tortoise = tortoise->next;
+            while (tortoise != hare)
+            {
+                printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+                count++;
+                tortoise = tortoise->next;
+            }
+            break;
+        }
 
-		count++;
-	}
+        count++;
+    }
 
-	return (count);
+    return count;
 }
 
