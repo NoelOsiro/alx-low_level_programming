9298a59eac64947bd15e0172e23cfbe2384c1c54
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-05-02 22:28:55 +0000

    102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,41 +1,41 @@
+
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t list safely (ie.
- *					 can free lists containing loops)
- * @h: A pointer to the address of
- *	 the head of the listint_t list.
+ * print_listint_safe - prints a listint_t linked list safely
+ * @head: pointer to the head node of the list
  *
- * Return: The size of the list that was freed.
- *
- * Description: The function sets the head to NULL.
+ * Return: the number of nodes in the list
  */
-
-size_t free_listint_safe(listint_t **h)
+size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *current, *temp;
+	const listint_t *tortoise = head, *hare = head;
 	size_t count = 0;
 
-	if (h == NULL || *h == NULL)
-		return (0);
-
-	current = *h;
-	while (current != NULL)
+	while (tortoise != NULL && hare != NULL && hare->next != NULL)
 	{
-		count++;
-		if (current < current->next)
+		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (tortoise == hare)
 		{
-			temp = current;
-			current = current->next;
-			free(temp);
-		}
-		else
-		{
-			free(current);
-			*h = NULL;
+			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+			count++;
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+				count++;
+				tortoise = tortoise->next;
+			}
 			break;
 		}
+
+		count++;
 	}
 
 	return (count);
 }
+
