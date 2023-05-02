1b5575ce35bcb2afb76e2e86901f2f3ac1755f43
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-05-02 22:31:43 +0000

    102

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,39 +1,65 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * print_listint_safe - prints a listint_t linked list safely
- * @head: pointer to the head node of the list
+ * _ra - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
+ */
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
+ * free_listint_safe - frees a listint_t linked list.
+ * @head: double pointer to the start of the list
  *
  * Return: the number of nodes in the list
  */
-size_t print_listint_safe(const listint_t *head)
+size_t free_listint_safe(listint_t **head)
 {
-	const listint_t *tortoise = head, *hare = head;
-	size_t count = 0;
+	size_t i, num = 0;
+	listint_t **list = NULL;
+	listint_t *next;
 
-	while (tortoise != NULL && hare != NULL && hare->next != NULL)
+	if (head == NULL || *head == NULL)
+		return (num);
+	while (*head != NULL)
 	{
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		tortoise = tortoise->next;
-		hare = hare->next->next;
-
-		if (tortoise == hare)
+		for (i = 0; i < num; i++)
 		{
-			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
-			count++;
-			tortoise = tortoise->next;
-			while (tortoise != hare)
+			if (*head == list[i])
 			{
-				printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-				count++;
-				tortoise = tortoise->next;
+				*head = NULL;
+				free(list);
+				return (num);
 			}
-			break;
 		}
-
-		count++;
+		num++;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
-
-	return (count);
+	free(list);
+	return (num);
 }
