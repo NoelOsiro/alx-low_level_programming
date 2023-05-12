706045b204110cdc946cd04342d864f03f8b68f9
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-05-12 18:22:44 +0000

    Added readme

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,37 +3,44 @@
 #include <unistd.h>
 #define BUF_SIZE 1024
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL) {
-        return 0;
-    }
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+	{
+		return (0);
+	}
 
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        return 0;
-    }
+	FILE *file = fopen(filename, "r");
 
-    char buffer[BUF_SIZE];
-    ssize_t total_read = 0;
-    ssize_t bytes_read;
+	if (file == NULL)
+	{
+		return (0);
+	}
 
-    while (total_read < letters && (bytes_read = fread(buffer, 1, BUF_SIZE, file)) > 0) {
-        ssize_t bytes_to_write = (letters - total_read) < bytes_read ? (letters - total_read) : bytes_read;
-        ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_to_write);
-        
-        if (bytes_written < 0) {
-            fclose(file);
-            return 0;
-        }
+	char buffer[BUF_SIZE];
+	ssize_t total_read = 0;
+	ssize_t bytes_read;
 
-        total_read += bytes_written;
+	while (total_read < letters &&
+		(bytes_read = fread(buffer, 1, BUF_SIZE, file)) > 0)
+	{
+		ssize_t bytes_to_write =
+			(letters - total_read) < bytes_read ? (letters - total_read) : bytes_read;
+		ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_to_write);
 
-        if (bytes_written != bytes_to_write) {
-            fclose(file);
-            return 0;
-        }
-    }
+		if (bytes_written < 0)
+		{
+			fclose(file);
+			return (0);
+		}
+		total_read += bytes_written;
+		if (bytes_written != bytes_to_write)
+		{
+			fclose(file);
+			return (0);
+		}
+	}
 
-    fclose(file);
-    return total_read;
+	fclose(file);
+	return (total_read);
 }
