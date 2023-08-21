3a37b29fe7ea2263ac5251bedb098f0ae81b1af8
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-08-21 21:59:26 +0000

    renamed to main

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/NoelOsiro/alx-low_level_programming/raw/master/0x18-dynamic_libraries/makemewin.so
+wget -P /tmp https://github.com/NoelOsiro/alx-low_level_programming/raw/main/0x18-dynamic_libraries/makemewin.so
 export LD_PRELOAD=/tmp/makemewin.so
